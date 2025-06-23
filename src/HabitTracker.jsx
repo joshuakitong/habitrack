412307676ce397e101c778ae895509cdf70275f4
joshuakitong <joshuakitong@gmail.com>
2025-06-24 03:26:11 +0800

    Initial commit for habit tracker app

---
+++ src/HabitTracker.jsx
@@ -0,0 +1,9 @@
+function HabitTracker() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-800">
+      <h1 className="text-4xl font-bold text-blue-400">Habit Tracker</h1>
+    </div>
+  );
+}
+
+export default HabitTracker;
