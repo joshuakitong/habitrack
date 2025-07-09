6b057f00873d4ca29f5e9ec1d58ec4277c1810d0
joshuakitong <joshuakitong@gmail.com>
2025-07-10 04:07:52 +0800

    fix: collapse nav bar on mobile

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,18 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
+import { Menu, X } from "lucide-react";
 
 const Navbar = () => {
-  const linkBase = "pb-2 border-b-4";
+  const [menuOpen, setMenuOpen] = useState(false);
+  const toggleMenu = () => setMenuOpen(!menuOpen);
+
+  const linkBase = menuOpen ? "border-b-4 w-fit" : "pb-2 border-b-4 w-fit";
   const linkActive = "border-gray-300";
   const linkInactive = "border-transparent hover:border-gray-500";
 
   return (
-    <nav className="flex justify-between items-center pt-4 mx-2 text-white border-b border-gray-300">
-      <div className="flex space-x-6">
+    <nav className="flex flex-col md:flex-row md:justify-between md:items-center pt-4 mx-2 text-white border-b border-gray-300">
+      <div className="flex justify-between items-center">
         <NavLink
           to="/"
           end
@@ -17,13 +22,26 @@ const Navbar = () => {
         >
           Habit Tracker
         </NavLink>
+
+        <button
+          className="md:hidden text-white focus:outline-none -mt-[0.5rem]"
+          onClick={toggleMenu}
+        >
+          {menuOpen ? <X size={24} /> : <Menu size={24} />}
+        </button>
       </div>
-      <div className="flex space-x-6">
+
+      <div
+        className={`flex-col md:flex-row md:flex md:space-x-6 mt-2 md:mt-0 ${
+          menuOpen ? "flex" : "hidden"
+        }`}
+      >
         <NavLink
           to="/habits"
           className={({ isActive }) =>
             `${linkBase} ${isActive ? linkActive : linkInactive}`
           }
+          onClick={() => setMenuOpen(false)}
         >
           Habits
         </NavLink>
@@ -32,6 +50,7 @@ const Navbar = () => {
           className={({ isActive }) =>
             `${linkBase} ${isActive ? linkActive : linkInactive}`
           }
+          onClick={() => setMenuOpen(false)}
         >
           Overview
         </NavLink>
@@ -40,6 +59,7 @@ const Navbar = () => {
           className={({ isActive }) =>
             `${linkBase} ${isActive ? linkActive : linkInactive}`
           }
+          onClick={() => setMenuOpen(false)}
         >
           Settings
         </NavLink>
