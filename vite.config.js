06bc6193eef56a3d89650b6efa6247839a98e4df
joshuakitong <joshuakitong@gmail.com>
2025-07-11 07:30:56 +0800

    build: deploy to firebase

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import fs from 'fs';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/habit-tracker-app/',
+  base: '/',
   server: {
     https: {
       key: fs.readFileSync('localhost-key.pem'),
